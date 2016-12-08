6ab49c577d1e384b3ebdee704c08834a3c4d5faa
BladeMight <BladeMight@gmail.com>
2016-12-08 05:56:59 +0200

    :memo: Changed version. :bug: Changed Main window textboxe's background color.

--- Mahou/MahouForm.Designer.cs
+++ Mahou/MahouForm.Designer.cs
@@ -150,6 +150,7 @@
         	// 
         	// tbCLHK
         	// 
+        	this.tbCLHK.BackColor = System.Drawing.SystemColors.Window;
         	this.tbCLHK.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
         	this.tbCLHK.Location = new System.Drawing.Point(136, 12);
         	this.tbCLHK.Name = "tbCLHK";
@@ -162,6 +163,7 @@
         	// 
         	// tbCSHK
         	// 
+        	this.tbCSHK.BackColor = System.Drawing.SystemColors.Window;
         	this.tbCSHK.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
         	this.tbCSHK.Location = new System.Drawing.Point(136, 37);
         	this.tbCSHK.Name = "tbCSHK";
@@ -295,6 +297,7 @@
         	// 
         	// tbCLineHK
         	// 
+        	this.tbCLineHK.BackColor = System.Drawing.SystemColors.Window;
         	this.tbCLineHK.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
         	this.tbCLineHK.Location = new System.Drawing.Point(136, 62);
         	this.tbCLineHK.Name = "tbCLineHK";
