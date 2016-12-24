1681ccac0f4d7d7eaa35486b217859ffbaf3fd93
BladeMight <BladeMight@gmail.com>
2016-12-24 14:58:32 +0200

    :memo: Changed targeting CPU to AnyCPU, so Mahou now will use all avaible CPUs which will improve performance.

--- Mahou/buildMahou.cmd
+++ Mahou/buildMahou.cmd
@@ -1,1 +1,1 @@
-@%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\Mahou.sln /t:Build /p:Configuration=Release
+@%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\Mahou.sln /nologo /m /t:Build /p:Configuration=Release /p:Platform="Any CPU"
