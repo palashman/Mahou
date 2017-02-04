2ed1dfc3972ef1d1cb5c5f2d1e5f42cbf3803785
BladeMight <BladeMight@gmail.com>
2017-02-04 08:31:56 +0200

    :memo: Improved error handling and added timeout for file write wait.

--- Mahou/Classes/Logging.cs
+++ Mahou/Classes/Logging.cs
@@ -1,5 +1,6 @@
 ﻿// Created by BladeMight in 05.12.2016-18:55
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 using System.IO;
 
@@ -10,7 +11,7 @@ namespace Mahou
 		readonly static string logdir = Path.Combine(Update.nPath, "Logs");
 		readonly static string log = Path.Combine(logdir, DateTime.Today.ToString("yyyy.MM.dd") + ".log");
 		static int errcount = 0; // Logging errors count
-		static bool messagebox = false; // To prevent multiple messageboxes
+		static bool messagebox = false;	// To prevent multiple messageboxes
 		/// <summary>
 		/// Write message to log.
 		/// </summary>
@@ -18,32 +19,51 @@ namespace Mahou
 		/// <param name="msgtype">Type of message: 0(or nothing) = Info, 1 = Error, 2 = Warning.</param>
 		public static void Log(string logmsg, int msgtype = 0)
 		{
-			if (MMain.MyConfs.ReadBool("Functions", "Logging")) { 
-				if (!Directory.Exists(Path.Combine(Update.nPath, "Logs")))
-					Directory.CreateDirectory(logdir);
-				var messagetype = "Info";
-				var msgtime = DateTime.Now.ToString("hh:mm:ss.fff");
-				switch (msgtype) {
-					case 1:
-						messagetype = "Error"; break;
-					case 2:
-						messagetype = "Warning"; break;
-				}
-				try
-				{
-				File.AppendAllText(log, msgtime + " [" + messagetype + "]\r\n                    " + logmsg + "\r\n");
-				}
-				catch (Exception e) {
-					if (errcount > 10 && !messagebox)
-					{
-						messagebox = true;
-						MessageBox.Show(MMain.Msgs[14]+"\r\n\r\n"+e.Message+"\r\n\r\n"+e.StackTrace,MMain.Msgs[5]);
-						MMain.MyConfs.Write("Functions","Logging","False");
-						errcount=0;
-						messagebox = false;
+				if (MMain.MyConfs.ReadBool("Functions", "Logging")) { 
+					if (!Directory.Exists(Path.Combine(Update.nPath, "Logs")))
+						Directory.CreateDirectory(logdir);
+					var messagetype = "Info";
+					var msgtime = DateTime.Now.ToString("hh:mm:ss.fff");
+					switch (msgtype) {
+						case 1:
+							messagetype = "Error";
+							break;
+						case 2:
+							messagetype = "Warning";
+							break;
 					}
-					errcount++;
-				}
+					var tologmsg = msgtime + " [" + messagetype + "]\r\n                    " + logmsg + "\r\n";
+					Stopwatch stopwatch = new Stopwatch();
+					stopwatch.Start();
+					while (true)
+					{
+						try {
+							#if DEBUG
+							Debug.WriteLine(tologmsg);
+							#else
+							File.AppendAllText(log, tologmsg);
+							#endif
+							break;
+						} catch (Exception e) {
+							Debug.WriteLine( msgtime +" [LogError]\r\n                    " + e.Message + "\r\n" + e.StackTrace + "\r\n");
+							if (errcount > 10 && !messagebox) {
+								messagebox = true;
+								MessageBox.Show(MMain.Msgs[14] + "\r\n\r\n" + e.Message + "\r\n\r\n" + e.StackTrace, MMain.Msgs[5]);
+								MMain.MyConfs.Write("Functions", "Logging", "False");
+								errcount = 0;
+								messagebox = false;
+							}
+							errcount++;
+						}						
+			            if (stopwatch.ElapsedMilliseconds > 50)
+			            {
+			            	Debug.WriteLine(msgtime + " Last logging cancelled...");
+			                break;
+			            }
+			            //Wait to Retry
+			            System.Threading.Thread.Sleep(5);
+			        }
+			        stopwatch.Stop();
 			}
 		}
 	}
