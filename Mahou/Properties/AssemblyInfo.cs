d559a35bed17b2dae0d041d4cb303f08484efeca
BladeMight <BladeMight@gmail.com>
2017-03-18 23:30:49 +0200

    :memo: Changed version.

--- Mahou/Properties/AssemblyInfo.cs
+++ Mahou/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Resources;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("2.1.0.9")]
-[assembly: AssemblyFileVersion ("2.1.0.9")]
+[assembly: AssemblyVersion ("2.1.1.1")]
+[assembly: AssemblyFileVersion ("2.1.1.1")]
 [assembly: NeutralResourcesLanguage ("")]
 [assembly: AssemblyDefaultAlias ("BladeMight")]
