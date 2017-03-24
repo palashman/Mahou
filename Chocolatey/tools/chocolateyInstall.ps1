bdada6ce3d381d3694a27d9dc573fcd153da1073
BladeMight <BladeMight@gmail.com>
2017-03-24 12:06:44 +0200

    :chocolate_bar: Updated sha.

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyZipPackage 'Mahou' 'https://github.com/BladeMight/Mahou/releases/download/v2.1.1.6/Mahou-v2.1.1.6.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -Checksum "fdcc05c2c82ee51c44bd7f0de493f44e300fb96ec16cc39f6dd5b9684f81411b"
+Install-ChocolateyZipPackage 'Mahou' 'https://github.com/BladeMight/Mahou/releases/download/v2.1.1.6/Mahou-v2.1.1.6.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -Checksum "5bdd45806ba78de18b0ee0ee2beabb407a15aa013b0bf6981202958cac8c2bbf"
