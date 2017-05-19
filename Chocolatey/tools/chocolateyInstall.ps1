6c7ac22ef1f64a1250be15b3275d0a370cbe950a
BladeMight <BladeMight@gmail.com>
2017-05-19 13:08:29 +0300

    :chocolate_bar: Fixed sum-check.

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyZipPackage 'Mahou' 'https://github.com/BladeMight/Mahou/releases/download/v2.1.2.6/Mahou-v2.1.2.6.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -CheckSum c5d133ad033545801bfd21541ec11d975aa15c12a776f73ab4704c7152d559dd 
+Install-ChocolateyZipPackage 'Mahou' 'https://github.com/BladeMight/Mahou/releases/download/v2.1.2.6/Mahou-v2.1.2.6.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -CheckSum fd00ed6cb757cf4983d67694d499b234 
