e816bbb6c785bf3900154933ea2f728d21ad8e62
BladeMight <BladeMight@gmail.com>
2017-09-10 20:26:58 +0300

    :memo: Updated build AS_dict script.

--- build-AS_dict.sh
+++ build-AS_dict.sh
@@ -5,9 +5,10 @@ if [[ "$1" == "" ]]; then
 	echo If pass {type} as all it will combine all dictionaries to AS_dict.txt.
 	echo -e "Possible {type}s(left to filename):\n"
 else
-	> ../AS_dict.t # temp
-	iconv -c -f utf-8 -t utf-8 ../AS_dict.t
-	file -i AS_dict.t
+	echo Creating empty AS_dict.txt
+	> ../AS_dict.txt # temp
+	echo Converting AS_dict.txt to UTF-8.
+	iconv -c -f utf-8 -t utf-8 ../AS_dict.txt
 fi
 all=0
 changed=0
@@ -17,33 +18,30 @@ fi
 for f in *.txt; do
 	if [[ "$f" == *"big"*  ]]; then
 		if [[ "${1,,}" == "big" ]] || [[ "${2,,}" == "big" ]] || [[ "${3,,}" == "big" ]] || [[ $all == 1 ]];  then
-			echo Adding $f to dict...
+			echo "-->Adding $f to AS_dict.txt..."
 			changed=1
-			cat $f >> ../AS_dict.t
-		else
+			cat $f >> ../AS_dict.txt
+		elif [[ "$1" == "" ]]; then
 			echo BIG: $f
 		fi
 	elif [[ "$f" == *!* ]]; then
 		if [[ "${1,,}" == "tiny" ]] || [[ "${2,,}" == "tiny" ]] || [[ "${3,,}" == "tiny" ]] || [[ $all == 1 ]]; then
-			echo Adding $f to dict...
+			echo "-->Adding $f to AS_dict.txt..."
 			changed=1
-			cat $f >> ../AS_dict.t
-		else
+			cat $f >> ../AS_dict.txt
+		elif [[ "$1" == "" ]]; then
 			echo tiny: $f
 		fi
 	else
 		if [[ "${1,,}" == "custom" ]] || [[ "${2,,}" == "custom" ]] || [[ "${3,,}" == "custom" ]] || [[ $all == 1 ]]; then
-			echo Adding $f to dict...
+			echo "-->Adding $f to AS_dict.txt..."
 			changed=1
-			cat $f >> ../AS_dict.t
-		else
+			cat $f >> ../AS_dict.txt
+		elif [[ "$1" == "" ]]; then
 			echo Custom: $f
 		fi
 	fi
 done
 if [[ $changed == 1 ]]; then
-	cd ..
-	# echo Converting to utf-8...
-	# iconv -f `file -i AS_dict.t | awk 'match($0,".*charset=(.*)", a) {print a[1]}'` -t utf-8 AS_dict.t > AS_dict.txt
-	# mv AS_dict.t AS_dict.txt
+	echo Done.
 fi
