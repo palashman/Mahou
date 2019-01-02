f8c4c0d010de48a97f448c5ccc59cc0c9bb31eea
BladeMight <BladeMight@gmail.com>
2019-01-02 23:41:53 +0200

    :chocolate_bar: Chocolatey install script updated to v2.9.0.0.

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyZipPackage 'Mahou' 'https://github.com/BladeMight/Mahou/releases/download/v2.8.0.0/Mahou-v2.8.0.0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -Checksum "0f6d5f430a5f58d9a6dbcd44d6ea175d"
+Install-ChocolateyZipPackage 'Mahou' 'https://github.com/BladeMight/Mahou/releases/download/v2.9.0.0/Mahou-v2.9.0.0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -Checksum "27717583169122b19235e9ef0f623c6a"
