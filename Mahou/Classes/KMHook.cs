d321edaa83e6abc5a56578b495ce5c7f79cb6cce
BladeMight <BladeMight@gmail.com>
2019-01-06 22:01:25 +0200

    :bug: Fixed clearing of LAST WORDS by Win key. affects #224

--- Mahou/Classes/KMHook.cs
+++ Mahou/Classes/KMHook.cs
@@ -337,7 +337,7 @@ namespace Mahou {
 				if (Key == Keys.Home || Key == Keys.End ||
 				    (Key == Keys.Tab && MMain.mahou.SnippetsExpandType != "Tab" && snipps.Length > 0) || Key == Keys.PageDown || Key == Keys.PageUp ||
 				   Key == Keys.Left || Key == Keys.Right || Key == Keys.Down || Key == Keys.Up ||
-				   Key == Keys.BrowserSearch || ((win||win_r) && (Key >= Keys.D1 || Key <= Keys.D9)) ||
+				   Key == Keys.BrowserSearch || ((win||win_r) && (Key >= Keys.D1 && Key <= Keys.D9)) ||
 				   ((ctrl||win||alt||ctrl_r||win_r||alt_r) && (Key != Keys.Menu  && //Ctrl modifier and key which is not modifier
 							Key != Keys.LMenu &&
 							Key != Keys.RMenu &&
